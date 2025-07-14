adee03f7c7c086cb1d630658d7658cca64bbd634
Arhan Dasmunshi <arhandm2004@gmail.com>
2025-07-14 19:59:00 +0530

    Add 5_webAPI_Microservice project

---
+++ 5_webAPI_Microservice/webAPI/ChatProducer/Program.cs
@@ -0,0 +1,20 @@
+﻿using Confluent.Kafka;
+
+var config = new ProducerConfig
+{
+    BootstrapServers = "localhost:9092"
+};
+
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+Console.WriteLine("Chat Producer started. Type your messages:");
+
+while (true)
+{
+    string? message = Console.ReadLine();
+    if (!string.IsNullOrWhiteSpace(message))
+    {
+        await producer.ProduceAsync("chat-app", new Message<Null, string> { Value = message });
+        Console.WriteLine($"[Sent]: {message}");
+    }
+}
